cb119b2fb03b365a23eb2b9085d7b4985056491d
arturp999 <arturpe95@gmail.com>
2021-07-16 22:13:17 +0100

    stayed at 7.0

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace OOP
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("Hello World!");
+
+
+            Shape t = new Shape();
+            //Console.WriteLine(t.Area(15, 20, "Rectangle"));
+
+            Console.WriteLine(Shape.ReturningObjShape());
+
+           
+
         }
     }
 }
---
+++ Shape.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class Shape
+    {
+        public string name { get; set; }
+
+        protected double width, length;
+
+        private int id;
+        private int result;
+        public const int SHAPE_TYPE = 1;
+
+      
+        public Shape(double dWidth, double dLength)
+        {
+            this.id = 99;
+            this.width = dWidth;
+            this.length = dLength;
+        }
+
+        public Shape()
+        {
+        }
+
+        public int Area(int width, int length, string type)
+        {
+            if (type == "Square")
+            {
+                result = length * 4;
+            }
+
+            if (type == "Rectangle")
+            {
+                result = length * width;
+            }
+
+            return result;
+        }
+
+        public static int ReturningObjShape()
+        {
+            return SHAPE_TYPE;
+        }
+       
+
+    }
+}
